f9dee0f43b8f9548be1d67288f9223c3695059d0
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-07-29 07:57:26 +0700

    fix: resolve error message

--- utils/bindData.go
+++ utils/bindData.go
@@ -4,15 +4,11 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golodash/galidator"
 )
 
 func BindData(obj interface{}, c *gin.Context) bool {
-	g := galidator.New()
-	customizer := g.Validator(obj)
-
 	if err := c.ShouldBindJSON(obj); err != nil {
-		ReturnResponse(http.StatusBadRequest, "Failed to Bind Data", "error", customizer.DecryptErrors(err), c)
+		ReturnResponse(http.StatusBadRequest, "Failed to Bind Data", "error", err.Error(), c)
 		return false
 	}
 	return true
